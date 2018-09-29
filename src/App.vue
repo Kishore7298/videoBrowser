a48471f179a92760e39b99cc2dbac0398d8fac7d
kishore <4nm15is025@nmamit.in>
2018-09-29 22:46:35 +0530

    Implementing Yotube API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
-        <SearchBar></SearchBar>
+        <SearchBar v-on:termChange="onTermChange"></SearchBar>
     </div>
 </template>
 <script>
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
+import axios from 'axios';
+const API_KEY = 'AIzaSyANdPNy4cxBaRN0QKwuNaNv-eoJq9kQGH0';
 export default {
     name:'App',
     components: {
         SearchBar
+    },
+    methods : {
+        onTermChange : function(searchTerm){
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q:searchTerm
+                }
+            }).then(response => console.log(response));
+        }
     }
 };
 </script>
